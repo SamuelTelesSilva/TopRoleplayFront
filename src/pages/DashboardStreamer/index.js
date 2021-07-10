8b9c4ef47febd8081ed05da5162466aec36ebc23
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-07-10 19:43:09 -0300

    Foi feito o form da pagina de Streamers e input de pesquisa

--- src/pages/DashboardStreamer/index.js
+++ src/pages/DashboardStreamer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, AreaForm, Form, AreaButton } from './styles';
+import { Container, AreaForm, Form, AreaButton, AreaContent } from './styles';
 import ButtonInput from '../../components/ButtonInput';
 
 const DashboardStreamer = () => {
@@ -8,31 +8,42 @@ const DashboardStreamer = () => {
 
     return(
         <Container>
-           <AreaForm>
-                <Form>
-                    <div className="title-input">Nome do  Streamer</div>
-                    <input type="text" placeholder="Digite o nome do streamer"/>
-                    <div className="title-input">Url imagem 1</div>
-                    <input type="text" placeholder="Url da imagem de capa"/>
-                    <div className="title-input">Url imagem 2</div>
-                    <input type="text" placeholder="Url da imagem para o Card"/>
-                    <div className="redeSociais">Redes Sociais</div>
-                    <div className="title-input">Instagram</div>
-                    <input type="text" placeholder="Url do Instagram"/>
-                    <div className="title-input">Discord</div>
-                    <input type="text" placeholder="Url do Discord"/>
-                    <div className="title-input">Twitter</div>
-                    <input type="text" placeholder="Url do Twitter"/>
-                </Form>
-                <AreaButton>
-                    <div className="button-register">
-                        <ButtonInput type="submit" value="Cadastrar"/>
+            <div className="aux-cont">
+                <AreaForm>
+                    <Form>
+                        <div className="title-input">Nome do  Streamer</div>
+                        <input className="input-form" type="text" placeholder="Digite o nome do streamer"/>
+                        <div className="title-input">Url imagem 1</div>
+                        <input className="input-form" type="text" placeholder="Url da imagem de capa"/>
+                        <div className="title-input">Url imagem 2</div>
+                        <input className="input-form" type="text" placeholder="Url da imagem para o Card"/>
+                        <div className="redeSociais">Redes Sociais</div>
+                        <div className="title-input">Instagram</div>
+                        <input className="input-form" type="text" placeholder="Url do Instagram"/>
+                        <div className="title-input">Discord</div>
+                        <input className="input-form" type="text" placeholder="Url do Discord"/>
+                        <div className="title-input">Twitter</div>
+                        <input className="input-form" type="text" placeholder="Url do Twitter"/>
+                    </Form>
+                    <AreaButton>
+                        <div className="button-register">
+                            <ButtonInput type="submit" value="Cadastrar"/>
+                        </div>
+                        <div className="button-update">
+                            <ButtonInput type="submit" value="Atualizar"/>
+                        </div>
+                    </AreaButton>
+                </AreaForm>
+                <AreaContent>
+                    <div className="search-content">
+                        <input className="input-search" type="text" placeholder="Digite o nome do Streamer"/>
+                        <div className="button-search">
+                            <input className="button-input-search" type="submit" value="Pesquisar"/>
+                        </div>
                     </div>
-                    <div className="button-update">
-                        <ButtonInput type="submit" value="Atualizar"/>
-                    </div>
-                </AreaButton>
-           </AreaForm>
+                    
+                </AreaContent>
+           </div>
         </Container>
     );
 }
--- src/pages/DashboardStreamer/styles.js
+++ src/pages/DashboardStreamer/styles.js
@@ -3,16 +3,30 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     width: 100%;
-    height:auto;
-    border: 1px solid red;
     justify-content: center;
+    height: 100%;
+
+    .aux-cont{
+        display: block;
+        width: 50%;
+        border: 1px solid red;
+        height: 100%;
+    }
+
+    @media screen and (max-width: 640px){ 
+        justify-content: start;
+
+        .aux-cont{
+            width: 100%;
+        }
+    }
+
 `;
 
 export const AreaForm = styled.div`
     display: block;
-    width: 50%;
-    height: 100%;
-    border: 1px solid white;
+    width: 100%;
+    height: auto;
 `;
 
 export const Form = styled.div`
@@ -27,7 +41,7 @@ export const Form = styled.div`
         margin: 16px 0px 16px 0px;
     }
 
-    input{
+    .input-form{
         width: 100%;
         height: 35px;
         border-radius: 15px;
@@ -57,4 +71,46 @@ export const AreaButton = styled.div`
     .button-update{
         width: 150px;
     }
+`;
+
+export const AreaContent = styled.div`
+    display: flex;
+    margin-top: 120px;
+
+    
+    .search-content{
+        display: flex;
+        width: 100%;
+    }
+
+    .input-search{
+        width: 80%;
+        height: 35px;
+        border-radius: 15px 0px 0px 15px;
+        padding: 0px 8px 0px 13px;
+        background-color: var(--corFundoPrincipal);
+        border: 2px solid var(--corTerciaria);
+        color: white;
+    }
+
+    .button-search{
+        width: 20%;
+    }
+
+    .button-input-search{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: var(--corTerciaria);
+        border-radius: 0px 5px 5px 0px;
+        color: white;
+        width: 100%;
+        height: 35px;
+        font-size: 16px;
+
+        :hover{
+            cursor: pointer;
+        }
+    }
+    
 `;
