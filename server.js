af9c55b0df56fa9b3d98767ee29edc75fcc46703
Samuel Teles <57865094+SamuelTelesSilva@users.noreply.github.com>
2021-10-04 17:34:20 -0300

    adicionando

--- server.js
+++ server.js
@@ -12,8 +12,18 @@ app.use('/',
     )
 )
 
-
 app.listen(process.env.PORT || 3000, (err) => {
     if(err) { return console.log(err)}
     console.log('Tudo funcionando certinho')
 })
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
